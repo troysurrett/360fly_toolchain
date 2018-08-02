2bc1455b7340fff9c5c677f09511c33f91c7b44e
Troy Surrett <troy.surrett@outlook.com>
2018-08-02 14:27:08 -0400

    A second step is required for davinci to read it
    for whatever reason, the output of the first command is not quite good enough for davinci. I did some googling and came
    up with this, which it does import. I did try making a combined version of this command, so it would remap and convert
    in 1 step, but the output was black and white. That doesn't make sense, but for some reason making it two steps Just
    Works.
    
    Also, get rid of the debugging code, i.e. "echo" - derp.

--- processing_script.sh
+++ processing_script.sh
@@ -1,5 +1,10 @@
 mkdir equirectangular
 for i in `ls ./*.MP4`
 do
-  echo "ffmpeg -i $i -i ~/git_repos/equirectangular-remap/fly360_x.pgm -i ~/git_repos/equirectangular-remap/fly360_y.pgm -lavfi remap ./equirectangular/$i"
+  ffmpeg -i $i -i ~/git_repos/equirectangular-remap/fly360_x.pgm -i ~/git_repos/equirectangular-remap/fly360_y.pgm -lavfi remap ./equirectangular/$i
 done
+cd equirectangular
+mkdir ready_for_davinci
+for i in `ls ./*.MP4`
+  ffmpeg -y -i $i -c:v mpeg4 -force_key_frames "expr:gte(t,n_forced*1)" -r ntsc-film -b:v 250000k -c:a pcm_s16le ./ready_for_davinci/$i
+do
