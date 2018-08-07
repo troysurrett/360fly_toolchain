1fb84bdb1821d87ae0a26cb28bcabad6ba781aa5
Troy Surrett <troy.surrett@outlook.com>
2018-08-07 09:41:21 -0400

    -c:v mpeg4 caused ffmpeg to complain the headers were damaged
    and it would refuse to work at all. I bashed my head on the wall for so friggin long trying to get it to run like that,
    forgetting I had made that change in an attempt to get Davinci to recognize the output off the hop. Holy frig.
    
    Oh, the second change about -b:v250000k in the blindspotting part is because I was noticing the bitrate dropped off
    during this step huge. I need to actually reduce that because the output filesize is larger than the input, but I need
    to research how to get that right.

--- processing_script.sh
+++ processing_script.sh
@@ -1,7 +1,7 @@
 mkdir equirectangular
 for i in `ls ./*.MP4`
 do
-  ffmpeg -i $i -c:v mpeg4 -i ~/git_repos/equirectangular-remap/fly360_x.pgm -i ~/git_repos/equirectangular-remap/fly360_y.pgm -lavfi remap ./equirectangular/$i
+  ffmpeg -i $i -i ~/git_repos/equirectangular-remap/fly360_x.pgm -i ~/git_repos/equirectangular-remap/fly360_y.pgm -lavfi remap -b:v 250000k ./equirectangular/$i
 done
 cd equirectangular
 mkdir ready_for_davinci
@@ -13,9 +13,5 @@ cd ready_for_davinci
 mkdir blindspotted
 for i in `ls ./*.MP4`
 do
-    # 190 * 2 = 380; 1440 + 190 = 1540; 
-  # ffmpeg -y -i $i -vf "pad=width=2880:height=1540:y=380,crop=w=2880:h=1440:y=190"  -f mp4 ./blindspotted/$i
-  #THIS IS CLOSER: you have to do some scaling. Just padding and cropping cuts out some sky, every time.
-  #ffmpeg -y -i ./FLY00101.mp4 -vf "pad=width=2880:height=4000:y=300:x=0,crop=w=2880:h=2000:y=150,scale=w=2880:h=1440" -f mp4 ./blindspotted/FLY00101_spotted_scaled.MP4
-  ffmpeg -y -i $i -vf "pad=width=4000:height=2000:y=0:x=-1,scale=h=1440,crop=w=2880:x=560" -f mp4 ./blindspotted/$i
+  ffmpeg -y -i $i -vf "pad=width=4000:height=2000:y=0:x=-1,scale=h=1440,crop=w=2880:x=560" -b:v 250000k -f mp4  ./blindspotted/$i
 done
