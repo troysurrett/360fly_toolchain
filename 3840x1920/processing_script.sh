6cad31fda6548ecb021802d4abb7fbf1c07bf35e
Troy Surrett <troy.surrett@outlook.com>
2018-08-07 09:44:07 -0400

    develop script to process into 3840x1920
    which is the maximum resolution that 360Fly Director will output

---
+++ 3840x1920/processing_script.sh
@@ -0,0 +1,19 @@
+mkdir equirectangular
+for i in `ls ./*.MP4`
+do
+  ffmpeg -i $i -i ~/git_repos/360fly_toolchain/3840x1920/fly360_3840x1920_x.pgm -i ~/git_repos/360fly_toolchain/3840x1920/fly360_3840x1920_y.pgm -lavfi remap ./equirectangular/$i
+done
+
+cd equirectangular
+mkdir ready_for_davinci
+for i in `ls ./*.MP4`
+do
+  ffmpeg -y -i $i -c:v mpeg4 -force_key_frames "expr:gte(t,n_forced*1)" -b:v 250000k -r 29.97 -f mp4 ./ready_for_davinci/$i
+done
+
+cd ready_for_davinci
+mkdir blindspotted
+for i in `ls ./*.MP4`
+do
+  ffmpeg -y -i $i -vf "pad=width=5334:height=2667:y=0:x=-1,scale=h=1920,crop=w=3840:x=747" -f mp4 ./blindspotted/$i
+done
